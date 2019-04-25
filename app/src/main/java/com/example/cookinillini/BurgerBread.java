337d4cb1862bba67079098b9f15895bf13be9794
Keertana Gunti <kgunti2@illinois.edu>
2019-04-25 14:32:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cookinillini/BurgerBread.java
@@ -0,0 +1,4 @@
+package com.example.cookinillini;
+
+public class BurgerBread {
+}
---
+++ app/src/main/java/com/example/cookinillini/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.cookinillini;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        burgerButton();
+    }
+
+
+    private void burgerButton() {
+        ImageButton one = (ImageButton) findViewById(R.id.TheAarthiBurger);
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BurgerBread.class));
+            }
+        });
+
+
+}
